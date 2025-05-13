b1e4b297f1eb2abb19720dc072e284b8e763f206
maja-yong <kys6034@naver.com>
2025-05-13 16:32:38 +0900

    주석

--- src/main/java/com/example/voteit/VoteitApplication.java
+++ src/main/java/com/example/voteit/VoteitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //안녕
 public class VoteitApplication {
-
+//웅
 	public static void main(String[] args) {
 		SpringApplication.run(VoteitApplication.class, args);
 	}
