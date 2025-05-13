3c42318d6393413f5f6a7bbc9e866d0b60a9faff
kangminju <yessuk5499@gmail.com>
2025-05-13 16:25:37 +0900

    first commit

---
+++ src/main/java/com/example/voteit/VoteitApplication.java
@@ -0,0 +1,13 @@
+package com.example.voteit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/voteit/VoteitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voteit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
