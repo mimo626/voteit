f655c439931ff4b2c7897f02127c927e1eceaa53
kangminju <yessuk5499@gmail.com>
2025-05-18 01:03:56 +0900

    메인페이지로 이동

--- src/main/java/com/example/voteit/controller/QuestionController.java
+++ src/main/java/com/example/voteit/controller/QuestionController.java
@@ -2,8 +2,15 @@ package com.example.voteit.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class QuestionController {
+
+    //메인 페이지
+    @GetMapping("/voteit/main")
+    public String mainPage() {
+        return "question/main";
+    }
 }
