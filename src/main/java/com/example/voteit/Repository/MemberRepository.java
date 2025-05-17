17b2ee87acf25a2f73bcef8ecc8b9966aa2e8954
kangminju <yessuk5499@gmail.com>
2025-05-18 01:37:51 +0900

    findAll 오버라이드 코드 추가

--- src/main/java/com/example/voteit/Repository/MemberRepository.java
+++ src/main/java/com/example/voteit/Repository/MemberRepository.java
@@ -4,11 +4,15 @@ import com.example.voteit.Entity.Member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long>{
 
     Optional<Member> findMemberByUserid(String userid);
+
+    @Override
+    List<Member> findAll();
 }
 
