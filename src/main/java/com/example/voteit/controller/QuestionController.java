43dd38ff3b44591d612ba36a26d4fa15f120b3e9
maja-yong <kys6034@naver.com>
2025-05-18 02:18:09 +0900

    main으로 넘어갈때 data들이 한번에 보이도록 코드 조금 추가!

--- src/main/java/com/example/voteit/controller/QuestionController.java
+++ src/main/java/com/example/voteit/controller/QuestionController.java
@@ -1,8 +1,12 @@
 package com.example.voteit.controller;
 
+import com.example.voteit.Entity.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -11,6 +15,14 @@ public class QuestionController {
     //메인 페이지
     @GetMapping("/voteit/main")
     public String mainPage() {
+        return "question/main";
+    }
+
+    @GetMapping("/voteit/main")
+    public String show(Model model) {
+        List<Member> questionList = questionRepository.findAll();
+
+        model.addAttribute("questionList", questionList);
         return "question/main";
     }
 }
