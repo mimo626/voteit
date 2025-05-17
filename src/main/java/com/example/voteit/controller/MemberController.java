da40312076f943a8b30da2b9d8c18093d4aeb131
kangminju <yessuk5499@gmail.com>
2025-05-18 01:39:06 +0900

    안 쓰는 import 제거

--- src/main/java/com/example/voteit/controller/MemberController.java
+++ src/main/java/com/example/voteit/controller/MemberController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
