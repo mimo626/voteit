842975452c54b437c042eefc2988581d9408570d
kangminju <yessuk5499@gmail.com>
2025-05-18 02:22:47 +0900

    findAll 오버라이드 코드 추가

--- src/main/java/com/example/voteit/Repository/QuestionRepository.java
+++ src/main/java/com/example/voteit/Repository/QuestionRepository.java
@@ -4,6 +4,11 @@ import com.example.voteit.Entity.Question;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+    @Override
+    List<Question> findAll();
 }
