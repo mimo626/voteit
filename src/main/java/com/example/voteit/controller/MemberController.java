09aa3fdf56484c524b3522726a1a763a0314459f
kangminju <yessuk5499@gmail.com>
2025-05-17 23:14:05 +0900

    경로명 수정

--- src/main/java/com/example/voteit/controller/MemberController.java
+++ src/main/java/com/example/voteit/controller/MemberController.java
@@ -1,9 +1,9 @@
 package com.example.voteit.controller;
 
 import org.springframework.ui.Model;
-import com.example.voteit.Entity.VoteIt;
-import com.example.voteit.Repository.VoteItRepository;
-import com.example.voteit.dto.VoteItForm;
+import com.example.voteit.Entity.Member;
+import com.example.voteit.Repository.MemberRepository;
+import com.example.voteit.dto.MemberForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,40 +13,40 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
-public class VoteItController {
+public class MemberController {
 
     @Autowired
-    VoteItRepository voteItRepository;
+    MemberRepository memberRepository;
 
-    //로그인 폼
-    @GetMapping("/login")
+    //회원가입 페이지
+    @GetMapping("/voteit/signup")
     public String login() {
-        return "login/new";
+        return "member/signup";
     }
 
     // 회원 등록 처리
-    @PostMapping("/join")
-    public String join(VoteItForm VoteItForm) {
+    @PostMapping("/voteit/join")
+    public String join(MemberForm memberForm) {
 
-        VoteItForm.logInfo();
+        memberForm.logInfo();
         //1. DTO를 Entity로 변환
-        VoteIt voteIt = VoteItForm.toEntity();
+        Member voteIt = memberForm.toEntity();
         voteIt.logInfo();
         //2. repository를 통해 DB로 엔터티를 저장
-        VoteIt saved = voteItRepository.save(voteIt);
+        Member saved = memberRepository.save(voteIt);
         saved.logInfo();
         return "";
     }
     //회원 정보 조회
-    @GetMapping("/VoteIt/{id}")
+    @GetMapping("/voteit/member/{id}")
     public String show(@PathVariable("id") Long id, Model model) {
         log.info("id = {} ", id);
         //1. id article
         //Optional<Article> article = articleRepository.findById(id);
-        VoteIt voteIt = voteItRepository.findById(id).orElse(null);
+        Member member = memberRepository.findById(id).orElse(null);
         //2.
-        model.addAttribute("voteIt", voteIt);
+        model.addAttribute("member", member);
         //3.
-        return "main/main";
+        return "member/detail";
     }
 }
