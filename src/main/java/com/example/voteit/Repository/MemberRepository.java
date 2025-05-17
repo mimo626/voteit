4ba7b9fd9623337c199e73865fc04efc36dc5a19
kangminju <yessuk5499@gmail.com>
2025-05-17 23:14:12 +0900

    MemberRepository로 이름 수정

--- src/main/java/com/example/voteit/Repository/MemberRepository.java
+++ src/main/java/com/example/voteit/Repository/MemberRepository.java
@@ -1,11 +1,11 @@
 package com.example.voteit.Repository;
 
-import com.example.voteit.Entity.VoteIt;
+import com.example.voteit.Entity.Member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VoteItRepository extends CrudRepository<VoteIt, Long>{
+public interface MemberRepository extends CrudRepository<Member, Long>{
 
 }
 
