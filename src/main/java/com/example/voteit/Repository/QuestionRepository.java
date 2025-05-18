9522952f21ae4373132a5da1871a2aeae0cbcf94
kangminju <yessuk5499@gmail.com>
2025-05-18 22:06:29 +0900

    findQuestionByTitle 추가

--- src/main/java/com/example/voteit/Repository/QuestionRepository.java
+++ src/main/java/com/example/voteit/Repository/QuestionRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+    Optional<Question> findQuestionByTitle(String title);
 
     @Override
     List<Question> findAll();
